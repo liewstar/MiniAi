ffb74006abd44a424f7842b352965efb21a46b27
retriblet <3098923672@qq.com>
2023-11-08 09:09:02 +0800

    更改环境变量和pom文件

--- MiniAiBackend/build.bat
+++
@@ -1,9 +0,0 @@
-set DATASOURCE_URL="jdbc:mysql://localhost:3306/miniai?useUnicode=true&characterEncoding=utf8"
-set DATASOURCE_USERNAME="root"
-set DATASOURCE_PASSWORD="liaoxin0328"
-
-set JWT_EXPIRE=604800
-set JWT_SECRET="12345678"
-set JWT_HEADER="HEADER"
-
-mvn install -f pom.xml
